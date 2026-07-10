f5c202e0ef4708f0aac28573d5d2fdbda44b5696
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-10 13:38:46 +0530

    Add files via upload

---
+++ Variables/sum.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		String bookname =" concept of physics";
+		String authorname =" H.C. varma";
+		int price =500;
+		System.out.println("We are selling a book whose name is " + bookname + ". It is a famous book whose author is " + authorname + ". Its price is  $" + price + ".");
+	}
+}
