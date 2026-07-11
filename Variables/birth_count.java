1c14158fffda92f4409e60ce41e37d3f5054aebc
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-11 22:58:07 +0530

    Add files via upload

---
+++ Variables/birth_count.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		int birthyear = 2006;
+		int currentyear = 2026;
+		int count = currentyear - birthyear ;
+		System.out.println(count);
+	}
+}
