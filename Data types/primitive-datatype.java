ec93a739b9bb34fc800b574ded85a5433e58577d
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 18:56:50 +0530

    Add files via upload

---
+++ Data types/primitive-datatype.java
@@ -0,0 +1,23 @@
+//Data types
+
+public class Datatypes {
+	public static void main(String[] args) {
+		byte age = 20;
+		short marks = 150;
+		int salary = 2000;
+		long population = 1000000000000l;
+		float pi = 3.14f;
+		double price = 19.9;
+		char grade = 'A' ;
+		boolean isStudent = true;
+		System.out.println( age);
+		System.out.println(marks);
+		System.out.println(salary);
+		System.out.println( population);
+		System.out.println( pi);
+		System.out.println( price);
+		System.out.println( grade);
+		System.out.println( isStudent);
+		
+	}
+}
