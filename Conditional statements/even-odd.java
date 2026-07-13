657983348f8585d0eec23e31e07d0b40e41c2fac
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-13 23:01:47 +0530

    Add files via upload

---
+++ Conditional statements/even-odd.java
@@ -0,0 +1,16 @@
+// check the number is even or odd
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner ( System.in);
+		int a = sc.nextInt( );
+		if(a% 2==0){
+			System.out.println("Even number");
+		}else{
+			System.out.println("Odd number");
+		}
+	}
+}
