d6204d0de68cbe7b4f6877ffbdfc2823fd086da6
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-16 22:47:52 +0530

    Add files via upload

---
+++ Loops/Multiplication-Table.java
@@ -0,0 +1,15 @@
+// make a multiplication table
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number = ");
+		
+		int n = sc.nextInt( );
+		for(int  i =1 ; i <=10 ; i++){
+		System.out.println( n + "*" +  i  + "=" +  (n * i ));
+		}	
+	}
+}
---
+++ Loops/num-100-To-1.java
@@ -0,0 +1,10 @@
+//Print number from 100 to 1
+
+public class Main {
+	public static void main(String[] args) {
+		int i = 1;
+		for(i =100; i>=1; i--){
+			System.out.println(i);
+		}
+	}
+}
